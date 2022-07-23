cadfe6198dae39ff6272e77f1a4599661dcdf347
Fajri-FxSx <108572333+Fajri-FxSx@users.noreply.github.com>
2022-07-23 17:35:36 +0700

    Add files via upload

---
+++ src/components/Card/VideoCard.tsx
@@ -0,0 +1,36 @@
+import { useRouter } from "next/router";
+import React, { FC } from "react";
+import ReactPlayer from "react-player";
+import VideoCard from "../../models/VideoCard";
+
+const VideoCard: FC<VideoCard> = ({ keyVideo }) => {
+  const router = useRouter();
+
+  if (!router.query.key) {
+    return (
+      <iframe
+        width={"100%"}
+        height='auto'
+        style={{ aspectRatio: "16/9", border: "none", borderRadius: "10px" }}
+        allowFullScreen={true}
+        src={keyVideo}
+      ></iframe>
+      // <p></p>
+    );
+  }
+
+  return (
+    <>
+      <ReactPlayer
+        url={`https://www.youtube.com/watch?v=${keyVideo}`}
+        playing={true}
+        width='100%'
+        height={"auto"}
+        style={{ aspectRatio: "16/9" }}
+        controls={true}
+      />
+    </>
+  );
+};
+
+export default VideoCard;
